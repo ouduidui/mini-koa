91c99872409b160103f1f0772711b9bacbdf3cc9
oushihao <oushihao97@163.com>
2021-12-22 13:29:12 +0800

    style: 格式化代码

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const router = new Router();
 
 // 中间件
 app.use(async (ctx, next) => {
-	const start = Date.now();
-	await next();
-	const end = Date.now();
-	console.log(`请求${ctx.url}耗时${end - start}ms`);
+  const start = Date.now();
+  await next();
+  const end = Date.now();
+  console.log(`请求${ctx.url}耗时${end - start}ms`);
 });
 
 // 配置静态文件服务
@@ -18,14 +18,14 @@ app.use(serve(__dirname + '/static'));
 
 // 设置路由
 router.get('/', async (ctx) => {
-	ctx.body = 'HelloWorld';
+  ctx.body = 'HelloWorld';
 });
 
 router.get('/about', async (ctx) => {
-	ctx.body = {
-		name: 'OUDUIDUI',
-		age: 18
-	};
+  ctx.body = {
+    name: 'OUDUIDUI',
+    age: 18
+  };
 });
 
 // 配置路由
@@ -34,5 +34,5 @@ app.use(router.routes());
 // 开启端口
 const PORT = 3000;
 app.listen(PORT, () => {
-	console.log(`miniKoa Listen ${PORT}`);
+  console.log(`miniKoa Listen ${PORT}`);
 });
